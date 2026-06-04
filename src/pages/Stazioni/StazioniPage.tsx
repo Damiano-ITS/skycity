8166e1c21d91542546aad296a87d872b51a9d8a2
DamianoITS <damiano.castellarin.24@stud.itsaltoadriatico.it>
2026-06-04 14:07:08 +0200

    feat: enforce uniqueness on name and ID with custom form validation banner

--- src/pages/Stazioni/StazioniPage.tsx
+++ src/pages/Stazioni/StazioniPage.tsx
@@ -169,7 +169,7 @@ export default function StazioniPage() {
   };
 
   if (view === 'create') {
-    return <NuovaStazioneForm onSave={handleCreateStazione} onCancel={() => setView('list')} />;
+    return <NuovaStazioneForm stazioni={stazioni} onSave={handleCreateStazione} onCancel={() => setView('list')} />;
   }
 
   if (id) {
